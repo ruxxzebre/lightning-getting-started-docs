ddab910f4841a2289ca438c260f3af38a537fd1d
ruxxzebre <paullaboratory@gmail.com>
2021-08-31 16:28:09 +0300

    Eslint + husky

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  rules: {
+    quotes: [2, 'single', 'avoid-escape'],
+    'no-extra-boolean-cast': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: true,
+        singleQuote: true,
+        tabWidth: 2,
+        printWidth: 100,
+      },
+    ],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
